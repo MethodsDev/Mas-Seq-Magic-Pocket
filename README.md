6fb2b7610f345625ac22bd7b38e3655553802aef
James Webber <jwebber@broadinstitute.org>
2023-01-13 14:11:06 -0500

    ran commit hooks

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # MasSeqViz
-MasSeqViz is a powerful tool for visualizing and analyzing isoform expression data generated using 
-Mas-Iso-Seq. With MasSeqViz, researchers can explore and compare isoform expression levels 
-across different samples and conditions, identify patterns and trends in the data, and gain a deeper 
-understanding of the functional impact of context-specific post-transcriptional regulation. Whether 
+MasSeqViz is a powerful tool for visualizing and analyzing isoform expression data generated using
+Mas-Iso-Seq. With MasSeqViz, researchers can explore and compare isoform expression levels
+across different samples and conditions, identify patterns and trends in the data, and gain a deeper
+understanding of the functional impact of context-specific post-transcriptional regulation. Whether
 you are working with bulk or single-cell isoform data, MasSeqViz provides an straightforward solution
-for visualizing and interpreting your results. MasSeqViz aim to help researchers using 
+for visualizing and interpreting your results. MasSeqViz aim to help researchers using
 Mas-Iso-Seq to study isoform expression and post-transcriptional regulation in their experiments.
--- src/mdl/masseqviz/vis_utils.py
+++ src/mdl/masseqviz/vis_utils.py
@@ -1,7 +1,8 @@
-import numpy as np
-import matplotlib.pylab as plt
-from typing import List, Tuple, Dict
 from itertools import groupby
+from typing import Dict, List, Tuple
+
+import matplotlib.pylab as plt
+import numpy as np
 
 
 def segment_binary_array(arr: np.ndarray) -> List[Tuple]:
@@ -15,20 +16,22 @@ def segment_binary_array(arr: np.ndarray) -> List[Tuple]:
 
 
 def annotate_axis_with_tracks(
-        feature_to_annotation_track_map: Dict[str, np.ndarray],
-        included_features: List[str],
-        feature_to_color_map: Dict[str, str],
-        ax: plt.Axes,
-        included_position_mask: np.ndarray,
-        alpha_region: float = 0.5,
-        alpha_line: float = 1.0,
-        lw: float = 1.):
+    feature_to_annotation_track_map: Dict[str, np.ndarray],
+    included_features: List[str],
+    feature_to_color_map: Dict[str, str],
+    ax: plt.Axes,
+    included_position_mask: np.ndarray,
+    alpha_region: float = 0.5,
+    alpha_line: float = 1.0,
+    lw: float = 1.0,
+):
     for feature in included_features:
-        segments = segment_binary_array(feature_to_annotation_track_map[feature][included_position_mask])
+        segments = segment_binary_array(
+            feature_to_annotation_track_map[feature][included_position_mask]
+        )
         color = feature_to_color_map[feature]
         for segment in segments:
             if segment[1] - segment[0] > 0:
                 ax.axvspan(segment[0], segment[1], color=color, alpha=alpha_region)
             else:
                 ax.axvline(segment[0], color=color, alpha=alpha_line, lw=lw)
-                
